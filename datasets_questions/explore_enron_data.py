aba19313b75c4bb387a4758c5caef556e038ba3b
Nathan <nmahoney@ualberta.ca>
2016-03-17 17:52:54 -0600

    updating git from lesson 6

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -17,6 +17,40 @@
 
 import pickle
 
-enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
 
+#print(enron_data['YEAGER F SCOTT'])
 
+#print(sum(enron_data[x]['poi'] for x in enron_data.keys() if enron_data[x]['poi'] == True))
+
+#print(enron_data.keys())
+
+#print(enron_data['PRENTICE JAMES'])
+
+#print(enron_data['PRENTICE JAMES']['total_stock_value'])
+
+#print(enron_data['SKILLING JEFFREY K']['total_payments'])
+#print(enron_data['FASTOW ANDREW S']['total_payments'])
+#print(enron_data['LAY KENNETH L']['total_payments'])
+
+#print(enron_data['SKILLING JEFFREY K']['email_address'])
+"""
+email = []
+salary = []
+
+salary = [enron_data[x]['salary'] for x in enron_data.keys() if enron_data[x]['salary'] != 'NaN']
+print(len(salary))
+
+email = [enron_data[x]['email_address'] for x in enron_data.keys() if enron_data[x]['email_address'] != 'NaN']
+print(len(email))
+"""
+
+total_payments = []
+total_payments = [enron_data[x] for x in enron_data.keys() if enron_data[x]['total_payments'] == 'NaN']
+print(total_payments)
+poi = []
+print(len(total_payments))
+print(len(enron_data))
+#poi = [total_payments[x] for x in 21 if "poi': False" in total_payments[x]]
+#print(len(poi))
+#print(len(total_payments)/len(enron_data.keys()))
