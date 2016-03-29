af52513b4df85f5fcb44c299793dba3785629b48
Nathan <nmahoney@ualberta.ca>
2016-03-29 07:22:12 -0600

    update files from lesson 8

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -8,12 +8,29 @@ from feature_format import featureFormat, targetFeatureSplit
 
 
 ### read in data dictionary, convert to numpy array
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "rb") )
+data_dict.pop('TOTAL', 0)
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
 
 ### your code below
 
+for x in data_dict.keys():
+    outlier = []
+    if data_dict[x]['salary'] != 'NaN':
+        if int(data_dict[x]['salary']) > 1000000:
+            outlier.append(x)
+    for y in outlier:
+        if data_dict[y]['bonus'] != 'NaN':    
+            if int(data_dict[y]['bonus']) > 4000000:
+                print(y)
 
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
 
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -15,6 +15,11 @@ def outlierCleaner(predictions, ages, net_worths):
 
     ### your code goes here
 
-    
-    return cleaned_data
+    # make a list of tuples
+    for x in range(0, len(predictions)):
+        new = ages[x], net_worths[x], abs(predictions[x] - net_worths[x])
+        cleaned_data.append(new)
+        
+    cleaned_data.sort(key = lambda tup: tup[2])
+    return cleaned_data[:81]
 
