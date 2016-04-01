f0dbf4117217175db8de280545c164a525b20d6c
Nathan <nmahoney@ualberta.ca>
2016-04-01 07:07:01 -0600

    Lesson 9 Feature Scaling results

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -54,12 +54,29 @@ features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
-#print(data)
+salary = []
+exercised_stock_options = []
+for y in range(len(data)):
+    if data[y][1] != 0:    
+        salary.append(data[y][1])
+    if data[y][2] != 0:
+        exercised_stock_options.append(data[y][2])
 
-list1 = [x for x in data[2]]
-print(max(list1))
-print(min(list1))
-    
+#print(max(list1))
+#print(min(list1))
+ 
+from sklearn.preprocessing import MinMaxScaler
+
+salary = numpy.array([[a] for a in salary])
+exercised_stock_options = numpy.array([[b] for b in exercised_stock_options])
+
+scaler1 = MinMaxScaler()
+scaler2 = MinMaxScaler()
+
+rescaled_salary = scaler1.fit_transform(salary)
+print(scaler1.transform(numpy.array([200000.])))
+rescaled_eso = scaler2.fit_transform(exercised_stock_options)   
+print(scaler2.transform(numpy.array([1000000.])))
 
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
